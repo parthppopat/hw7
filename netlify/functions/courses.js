e8180c8872655ff8b39c820fe7bee97d978fd040
Parth Popat <parth.popat@kellogg.northwestern.edu>
2021-06-05 21:40:15 -0500

    changes

--- netlify/functions/courses.js
+++ netlify/functions/courses.js
@@ -7,7 +7,7 @@
 // - Reviews can be written (anonymously) about the lecturer/course combination (what would that be called?)
 // - Reviews contain a String body, and a numeric rating from 1-5
 // - Keep it simple and ignore things like multiple course offerings and quarters; assume reviews are written
-//   about the lecturer/course combination only – also ignore the concept of a "user" and assume reviews
+//   about the lecturer/course combination only – also ignore the concept of a "user" and assume reviews
 //   are written anonymously
 //
 // Tasks:
@@ -48,7 +48,7 @@ exports.handler = async function(event) {
 
   // get the first document from the query
   let course = courseQuery.docs[0]
-
+  
   // get the id from the document
   let courseId = course.id
 
@@ -58,20 +58,24 @@ exports.handler = async function(event) {
   // create an object with the course data to hold the return value from our lambda
   let returnValue = {
     courseNumber: courseData.courseNumber,
-    name: courseData.name
+    name: courseData.name,
+    totalReviews: 0,
+    averageCourseRating:0
   }
 
   // set a new Array as part of the return value
   returnValue.sections = []
-
+  
   // ask Firebase for the sections corresponding to the Document ID of the course, wait for the response
   let sectionsQuery = await db.collection('sections').where(`courseId`, `==`, courseId).get()
 
   // get the documents from the query
   let sections = sectionsQuery.docs
+  
 
   // loop through the documents
   for (let i=0; i < sections.length; i++) {
+
     // get the document ID of the section
     let sectionId = sections[i].id
 
@@ -87,14 +91,59 @@ exports.handler = async function(event) {
     // get the data from the returned document
     let lecturer = lecturerQuery.data()
 
-    // add the lecturer's name to the section Object
+    // add the lecturer's name and courseID to the section Object
     sectionObject.lecturerName = lecturer.name
+    sectionObject.courseId=sectionData.courseId
 
     // add the section Object to the return value
     returnValue.sections.push(sectionObject)
 
     // 🔥 your code for the reviews/ratings goes here
+
+    // Create a new array to hold the reviews
+    sectionObject.reviews =[]
+
+    // Ask Firebase for the relevant reviews from the section ID, wait for the response
+    let reviewsQuery = await db.collection('reviews').where(`sectionId`, `==`, sectionId).get()
+
+    // Get the documents from the query
+    let reviews = reviewsQuery.docs
+
+    // Create a loop to go through all the reviews for the ID
+    for (let reviewIndex=0; reviewIndex < reviews.length; reviewIndex++) {
+    
+    // Get the review details from the document
+    let reviewDetails = reviews[reviewIndex].data()
+
+    // Create a review object and add it to the section object
+    let reviewObject = {
+      rating: reviewDetails.rating,
+      body: reviewDetails.body,
+      sectionId: reviewDetails.sectionId
+    }
+    
+    // Push review object into the reviews array
+    sectionObject.reviews.push(reviewObject)
+
+    // Add +1 to the review count
+    returnValue.totalReviews=returnValue.totalReviews+1    
+
+    // Find the review count
+    sectionObject.sectionReviewCount=sectionObject.reviews.length
   }
+    // Formula to caculate average rating: (1) loop through review ratings (2) add them up (3) divide by the length
+     var sum =0;
+     for(var x=0 ; x<reviews.length ; x++){
+        sum+=sectionObject.reviews[x].rating;
+      }
+    sectionObject.averageSectionRating=sum/sectionObject.reviews.length
+}
+    // Formula for weighted average: (1) Loop through section ratings and multiply by number of sections (2) divide by the number of reviews
+    var wtdSum =0;
+    for(var y=0 ; y<returnValue.sections.length ; y++){
+      wtdSum+=returnValue.sections[y].sectionReviewCount*returnValue.sections[y].averageSectionRating;
+    }
+    returnValue.averageCourseRating = wtdSum/returnValue.totalReviews
 
   // return the standard response
   return {
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyBDHjtC5F_ndaNWJ0U9-gHEfctyR29s2aE",
+  authDomain: "kiei-451-e377c.firebaseapp.com",
+  projectId: "kiei-451-e377c",
+  storageBucket: "kiei-451-e377c.appspot.com",
+  messagingSenderId: "91611427179",
+  appId: "1:91611427179:web:abe8a275a68beaf2ad1c2e"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
